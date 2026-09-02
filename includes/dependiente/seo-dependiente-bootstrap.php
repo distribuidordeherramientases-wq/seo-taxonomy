230326e2da6f187fbc521e2890424126e4651955
distribuidordeherramientases-wq <distribuidordeherramientas.es@gmail.com>
2026-09-02 19:41:26 +0200

    Academia  automática
    Academia  automática

--- includes/dependiente/seo-dependiente-bootstrap.php
+++ includes/dependiente/seo-dependiente-bootstrap.php
@@ -4,7 +4,7 @@
  */
 defined('ABSPATH') || exit;
 
-define('SEO_DEPENDIENTE_VERSION', '0.1.31');
+define('SEO_DEPENDIENTE_VERSION', '0.1.32');
 define('SEO_DEPENDIENTE_DB_VERSION', '0.1.3');
 define('SEO_DEPENDIENTE_PATH', __DIR__ . '/');
 define('SEO_DEPENDIENTE_URL', SEO_SYSTEM_URL . 'includes/dependiente/');
