397c564cb5b62b29b615bd36bc25e1bdf0d5a81a
distribuidordeherramientases-wq <distribuidordeherramientas.es@gmail.com>
2026-09-04 22:40:26 +0200

    Bump version to 2.3.3

--- seo-taxonomy.php
+++ seo-taxonomy.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: SEO Taxonomy
  * Description: Sistema SEO basado en relaciones.
- * Version: 2.3.2
+ * Version: 2.3.3
  * Requires PHP: 7.4
  * Requires at least: 5.8
  */
@@ -12,8 +12,8 @@ defined('ABSPATH') || exit;
 /**
  * VERSIONES
  */
-define('SEO_SYSTEM_VERSION', '2.3.2');
-define('SEO_SYSTEM_DB_VERSION', '2.3.2');
+define('SEO_SYSTEM_VERSION', '2.3.3');
+define('SEO_SYSTEM_DB_VERSION', '2.3.3');
 
 /**
  * RUTAS
